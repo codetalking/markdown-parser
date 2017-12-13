65b095369c6a8185b4e9d0b88a34d69f284499cd
benrong.jcy <benrong.jcy@antfin.com>
2017-12-13 17:52:01 +0800

    init

---
+++ test/markdown_parser/core_test.clj
@@ -0,0 +1,7 @@
+(ns markdown-parser.core-test
+  (:require [clojure.test :refer :all]
+            [markdown-parser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
